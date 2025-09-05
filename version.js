16467ae7151667d7883a0f53bec206167d43da77
barbosaj1983 <jeffersobsbarbosa.2010@gmail.com>
2025-09-05 18:18:37 -0400

    chore: bump version to 1.10.8

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Auto-generated version file
 module.exports = {
-    version: '1.10.7',
+    version: '1.10.8',
     buildDate: '2025-09-05',
-    buildTimestamp: 1757110442212
+    buildTimestamp: 1757110717540
 };
